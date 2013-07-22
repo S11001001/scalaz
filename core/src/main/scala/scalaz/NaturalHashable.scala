07441a6e7fde937fcd57f9390126b2c240e4c223
Stephen Compall <scompall@nocandysw.com>
2013-07-22 14:58:18 -0600

    The source of all eventual NaturalHashable evidence.

--- core/src/main/scala/scalaz/NaturalHashable.scala
+++ core/src/main/scala/scalaz/NaturalHashable.scala
@@ -2,11 +2,16 @@ package scalaz
 
 ////
 /**
- *
+ * Marker for types for which `Any.##`, and, by implication, `Any.==`,
+ * are well-defined.
  */
 ////
 trait NaturalHashable[F] extends Equal[F] { self =>
   ////
+
+  final def equal(a1: F, a2: F) = a1 == a2
+
+  override final def equalIsNatural = true
 
   // derived functions
 
@@ -18,6 +23,10 @@ object NaturalHashable {
   @inline def apply[F](implicit F: NaturalHashable[F]): NaturalHashable[F] = F
 
   ////
+  private[this] def iinstance = new NaturalHashable[Any]{}
 
+  /** Assert that `A` is naturally hashable. */
+  def instance[A]: NaturalHashable[A] =
+    iinstance.asInstanceOf[NaturalHashable[A]]
   ////
 }
